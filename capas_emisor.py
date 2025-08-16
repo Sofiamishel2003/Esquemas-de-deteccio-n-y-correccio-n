ff084079bcc0f8f7f652e471f114e460d478942e
DiegoGarV <dgv3105@gmail.com>
2025-08-16 01:40:41 -0600

    capa de enlace emisor

--- capas_emisor.py
+++ capas_emisor.py
@@ -1,11 +1,11 @@
 import socket
 import random
-import binascii
+import emisor
 
 # ====================== CAPA: Aplicación ======================
 def solicitar_mensaje():
     mensaje = input("Ingrese el mensaje a enviar: ")
-    algoritmo = input("Algoritmo de integridad (hamming, crc32, fletcher8): ").lower()
+    algoritmo = input(MENU).strip()
     return mensaje, algoritmo
 
 
@@ -14,11 +14,40 @@ def codificar_ascii(mensaje):
     return ''.join(format(ord(c), '08b') for c in mensaje)
 
 
+# ====================== CAPA: Enlace ======================
+def calcular_integridad(bits, algoritmo):
+    if algoritmo == "1":  # Hamming
+        return emisor.hamming_emit(bits)
+
+    elif algoritmo == "2":  # CRC-32
+        return emisor.crc32_emit(bits)
+
+    elif algoritmo == "3":  # Fletcher
+        tipo = input("Seleccione tipo Fletcher (8, 16, 32): ").strip()
+        if tipo not in ("8", "16", "32"):
+            raise ValueError("Tipo de Fletcher no válido")
+        frame, _, _ = emisor.fletcher_emit(bits, int(tipo))
+        return frame
+
+    else:
+        raise ValueError("Algoritmo no soportado")
+
+
 
 
 
 # ====================== MAIN ======================
 if __name__ == "__main__":
+    MENU = """
+Seleccione algoritmo:
+    1) Hamming (corrección de 1 bit)
+    2) CRC-32 (detección)
+    3) Fletcher checksum (8/16/32)
+Opción: """
     mensaje, algoritmo = solicitar_mensaje()
     bits = codificar_ascii(mensaje)
-    print(f"Mensaje codificado en bits: {bits}")
+    # print(f"\nMensaje en binario: {bits}")
+    trama = calcular_integridad(bits, algoritmo)
+    print(f"Trama generada: {trama}")
+    # trama_ruido = aplicar_ruido(trama, prob_error=0.01)
+    # enviar_trama(trama_ruido)
